b219bf9bf524bae330249d4aa388ac94f00fd18c
PHH <HungPH.B22CN367@stu.ptit.edu.vn>
2025-06-03 22:18:18 +0700

    format vnd

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 
-export const API_URL = 'https://d785-103-238-68-84.ngrok-free.app/api/';
+export const API_URL = 'https://6040-118-71-137-130.ngrok-free.app/api/';
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)],
 };
